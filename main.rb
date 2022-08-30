523a1d3a77f6ee7f3cb408e09c06e36a9106e57f
afyqeyzar <norafyqe@gmail.com>
2022-08-30 17:29:00 +0100

    added methods into class

--- main.rb
+++ main.rb
@@ -1,13 +1,36 @@
 class LinkedList
+  def initialize
+    @head = nil
+  end
 
+  def append(value)
+    new_node = Node.new(value)
+    last_node = @head
+    if last_node.nil?
+      @head = new_node
+    else
+      last_node = last_node.next_node until last_node.next_node.nil?
+      last_node.next_node = new_node
+    end
+    self
+  end
+
+  def prepend(value)
+    new_node = Node.new(value, @head)
+    @head = new_node
+    self
+  end
 end
 
 class Node
-  def value(@value = nil)
-    @value
-  end
+  attr_accessor :value, :next_node
 
-  def next_node(@next_node = nil)
-    @next_node
+  def initialize(value, next_node = nil)
+    @value = value
+    @next_node = next_node
   end
 end
+
+linked_list = LinkedList.new()
+linked_list.append(2)
+linked_list.append(3)
